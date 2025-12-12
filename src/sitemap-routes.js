d7e1d0283fe815c3469d280a82dda5ecd8cc0c6f
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2025-12-12 18:08:31 +0100

    Working on sitemaps

---
+++ src/sitemap-routes.js
@@ -0,0 +1,13 @@
+export default [
+    '/',
+    '/our-vision',
+    '/our-mission',
+    '/docuhealth-news',
+    '/docuhealth-api',
+    '/our-legal-notice',
+    '/our-privacy-policy',
+    '/user-login',
+    '/user-create-account',
+    '/user-forgot-password'
+  ];
+  
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import sitemap from "vite-plugin-sitemap";
+import routes from './src/sitemap-routes.js'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), 
-    // tailwindcss()
+    sitemap({
+      hostname: "https://docuhealthservices.net",
+      routes: routes,
+      readable: true,
+      exclude: [
+        "/dashboard*",
+        "/hospital-*",
+        "/admin*",
+      ],
+    }),
   ],
   
 })
