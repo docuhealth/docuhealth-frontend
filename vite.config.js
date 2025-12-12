0b863f6fe24b1b510144da0da3a09b1af9db1fff
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2025-12-12 18:23:28 +0100

    Changes to site

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import sitemap from "vite-plugin-sitemap";
-import routes from './src/sitemap-routes.js'
+
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), 
-    sitemap({
-      hostname: "https://docuhealthservices.net",
-      routes: routes,
-      readable: true,
-      exclude: [
-        "/dashboard*",
-        "/hospital-*",
-        "/admin*",
-      ],
-    }),
   ],
   
 })
