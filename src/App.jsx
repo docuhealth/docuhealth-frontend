4a9d483b3fd3339d3b9e60dcb16b6dcf85a62d2c
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2025-12-22 12:06:29 +0100

    Changes to website

--- src/App.jsx
+++ src/App.jsx
@@ -124,7 +124,7 @@ function App() {
 
   const hostname = window.location.hostname;
 
-  const isHospital = true || hostname.startsWith("hospital.");
+  const isHospital = hostname.startsWith("hospital.");
 
   return (
     <Router>
