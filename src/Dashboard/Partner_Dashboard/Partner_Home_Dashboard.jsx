0d8106af8ab9148dc81539b9647ddc129652d7ae
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-03-16 12:47:16 +0100

    Changes to mail on partners page

--- src/Dashboard/Partner_Dashboard/Partner_Home_Dashboard.jsx
+++ src/Dashboard/Partner_Dashboard/Partner_Home_Dashboard.jsx
@@ -156,10 +156,10 @@ const Partner_Home_Dashboard = () => {
             </div>
           </div>
           <a
-            href="mailto:support@docuhealthservices.com"
+            href="mailto:info@docuhealth.online"
             className="bg-white border border-[#3E4095] text-[#3E4095] px-6 py-2 rounded-full font-semibold hover:bg-[#3E4095] hover:text-white transition-all duration-300 text-sm text-center w-full md:w-auto"
           >
-            support@docuhealthservices.com
+            info@docuhealth.online
           </a>
         </div>
       </main>
