315f7bc034270964746372f81aeac450c318d434
Hermes <hermeschigoziri@gmail.com>
2026-05-08 15:13:30 +0100

    refactor api functions to support search params

--- src/queries/Hospital/admin/appointments.js
+++ src/queries/Hospital/admin/appointments.js
@@ -3,10 +3,16 @@
 import axiosInstanceHos from "../../../utils/axiosInstanceHos";
 
 export const fetchAppointments = async ({ queryKey }) => {
-  const [_key, page] = queryKey;
+  const [_key, page, search, dateFrom, dateTo] = queryKey;
   const pageSize = 7;
+
+  const params = new URLSearchParams({ page, size: pageSize });
+  if (search) params.append("search", search);
+  if (dateFrom) params.append("scheduled_time_gte", dateFrom);
+  if (dateTo) params.append("scheduled_time_lte", dateTo);
+
   const res = await axiosInstanceHos.get(
-    `api/hospitals/appointments?page=${page}&size=${pageSize}`,
+    `api/hospitals/appointments?${params.toString()}`,
   );
 
   return res.data;
