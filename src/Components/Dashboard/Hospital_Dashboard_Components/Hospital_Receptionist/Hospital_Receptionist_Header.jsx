fbab7c7076920f20dff1c0cce2b45467d69dfeed
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-06-04 17:44:53 +0100

    App jsx

--- src/Components/Dashboard/Hospital_Dashboard_Components/Hospital_Receptionist/Hospital_Receptionist_Header.jsx
+++ src/Components/Dashboard/Hospital_Dashboard_Components/Hospital_Receptionist/Hospital_Receptionist_Header.jsx
@@ -16,7 +16,6 @@ const Hospital_Receptionist_Header = () => {
   
       const {profile} = useContext(ReceptionistAppContext);
 
-      console.log(profile)
 
       const navigate = useNavigate()
 
