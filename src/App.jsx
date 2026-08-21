23f9f20acc1d30b7863c3d9d6aa6c30a41705085
Adebayo Favour Ayomide <fadebayo637@gmail.com>
2026-08-21 16:57:54 +0100

    Changes to the integration of the checkin system

--- src/App.jsx
+++ src/App.jsx
@@ -150,7 +150,7 @@ function App() {
   const hostname = window.location.hostname;
 
   //Adjust the condition to remove the true value when deploying to production. This is just for testing purposes to always render the hospital routes.
-  const isHospital = true || hostname.startsWith("hospital.");
+  const isHospital = hostname.startsWith("hospital.");
 
   return (
     <HelmetProvider>
