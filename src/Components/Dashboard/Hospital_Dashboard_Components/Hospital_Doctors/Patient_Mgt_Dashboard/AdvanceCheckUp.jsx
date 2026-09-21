3a42e51f128530e3bb44f1d4d4bcd592f16901fa
docuhealth <docuhealthservice@gmail.com>
2026-09-21 14:46:37 +0100

    Fix/registration flow location (#88)
    * Fix to the SOAP Note and commenting out Patient Medical records from the Patient MGT ( Doctors view)
    
    * Fix to the SOAP Note view on doctors dashboard ( to show only the patient's soap not ), commenting out Patient Medical records from the Patient MGT ( Doctors view), fixing picking location during registration ( Country, State, LGA )
    
    ---------
    
    Co-authored-by: Adebayo Favour Ayomide <fadebayo637@gmail.com>

--- src/Components/Dashboard/Hospital_Dashboard_Components/Hospital_Doctors/Patient_Mgt_Dashboard/AdvanceCheckUp.jsx
+++ src/Components/Dashboard/Hospital_Dashboard_Components/Hospital_Doctors/Patient_Mgt_Dashboard/AdvanceCheckUp.jsx
@@ -47,7 +47,7 @@ const AdvanceCheckUp = ({
     queryKey: ["patient-med-records", hin, currentPage],
     queryFn: async () => {
       const res = await axiosInstanceHos.get(
-        `api/doctors/patient/records/${hin}?page=${currentPage}&size=${pageSize}`,
+        `api/medical-records/all?patient=${hin}&page=${currentPage}&size=${pageSize}`,
       );
       return res.data;
     },
@@ -59,7 +59,7 @@ const AdvanceCheckUp = ({
     queryKey: ["patient-soap-notes", hin, soapCurrentPage],
     queryFn: async () => {
       const res = await axiosInstanceHos.get(
-        `api/medical-records/all?patient=${hin}&page=${soapCurrentPage}&size=${pageSize}`,
+        `api/doctors/patient/records/${hin}?page=${soapCurrentPage}&size=${pageSize}`,
       );
       return res.data;
     },
